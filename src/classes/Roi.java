bb4012c6f2ceefd3057a5880df424521d34c5444
RomaricDoumenc <31727524+RomaricDoumenc@users.noreply.github.com>
2018-01-26 23:14:43 +0100

    Mouvement des rois
    Implémenter le mouvement des rois

--- src/classes/Roi.java
+++ src/classes/Roi.java
@@ -9,12 +9,32 @@ public class Roi extends Piece {
 
 	@Override
 	public void seDeplacer(int xArr, int yArr) {
-		// TODO Auto-generated method stub
+		int xDep = this.x;
+		int yDep = this.y;
+		
+		if((Math.abs(xDep - xArr) == 0) || (Math.abs(xDep - xArr) == 1))
+			if((Math.abs(yDep - yArr) == 0) || (Math.abs(yDep - yArr) == 1))
+				if (trajectoireLibre(xDep, yDep, xArr, yArr) == true) { // Pas de pièce sur la trajectoire du fou ?
+					if(this.ech.getPieces()[xArr][yArr] == null) // Case libre ?
+						bougerPieceSurEchiquier(xDep, yDep, xArr, yArr); // Déplacement sur cette case
+					else if(this.ech.getPieces()[xArr][yArr].getCoul() != this.coul) {
+						// Pièce adverse sur la case d'arrivée ?
+						capturerAdversaire(xArr, yArr);
+						bougerPieceSurEchiquier(xDep, yDep, xArr, yArr);
+					}
+				}
+		
+		
 
 	}
 	
 	public boolean estEnEchec() { // Indique si le roi est en échec ou non
 		
+	}
+
+	@Override
+	protected boolean trajectoireLibre(int xDep, int yDep, int xArr, int yArr) {
+		return true;
 	}
 
 }
