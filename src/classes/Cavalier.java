a913e85d7ac0c2416a2db8fbfef78b3185184157
RomaricDoumenc <31727524+RomaricDoumenc@users.noreply.github.com>
2018-01-27 15:26:49 +0100

    Mouvement des cavaliers
    Implémenter le mouvement des cavaliers.

--- src/classes/Cavalier.java
+++ src/classes/Cavalier.java
@@ -13,6 +13,20 @@ public class Cavalier extends Piece {
 	
 		int xDep = this.x;
 		int yDep = this.y;
+		int deltaX = Math.abs(xDep - xArr);
+		int deltaY = Math.abs(yDep - yArr);
+		
+		if(((deltaX == 1) && (deltaY == 2)) || ((deltaX == 2) && (deltaY == 1))) {
+			if (trajectoireLibre(xDep, yDep, xArr, yArr) == true) { // Pas de pièce sur la trajectoire du fou ?
+				if(this.ech.getPieces()[xArr][yArr] == null) // Case libre ?
+					bougerPieceSurEchiquier(xDep, yDep, xArr, yArr); // Déplacement sur cette case
+				else if(this.ech.getPieces()[xArr][yArr].getCoul() != this.coul) {
+					// Pièce adverse sur la case d'arrivée ?
+					capturerAdversaire(xArr, yArr);
+					bougerPieceSurEchiquier(xDep, yDep, xArr, yArr);
+				}
+			}
+		}
 		
 		
 		
